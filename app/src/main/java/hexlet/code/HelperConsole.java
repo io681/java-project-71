445a2571b36859ce31b8bb8b31effa961179f23f
io681 <io-68@yandex.ru>
2023-11-16 11:30:59 +0300

    step4 - picocli

--- app/src/main/java/hexlet/code/HelperConsole.java
+++ app/src/main/java/hexlet/code/HelperConsole.java
@@ -3,17 +3,24 @@ package hexlet.code;
 //import picocli.CommandLine;
 //import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-//import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 //@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
 //        description = "Prints the checksum (SHA-256 by default) of a file to STDOUT.")
 public class HelperConsole implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
     @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
     private boolean helpRequested = false;
 
     @Option(names = { "-V", "--version" }, description = "Print version information and exit.")
     private boolean version = false;
+
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format = "=format";
 
     @Override
     public Integer call() throws Exception{
